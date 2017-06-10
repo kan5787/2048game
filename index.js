57df555817644073b0cdab5e9cdb6fef201a8c09
kan5787 <kan5787@sina.com>
2017-06-10 14:57:58 +0800

    随机生成数字

--- index.js
+++ index.js
@@ -12,11 +12,10 @@ var style = new PIXI.TextStyle({
 });
 
 var basicText = new PIXI.Text('2048',style);
-basicText.x = window.innerWidth/12;
-basicText.y = window.innerWidth/12*3;
+basicText.x = app.renderer.width/12;
+basicText.y = app.renderer.height/5;
 
-app.stage.addChild(basicText)
-
+app.stage.addChild(basicText);
 var grid=[];
 for(var i=0;i<4;i++){
     grid[i]=[0,0,0,0]
@@ -30,3 +29,24 @@ for(var i=0;i<4;i++){
         app.stage.addChild(graphics);
     }
 }
+
+function generateRomderNumber() {
+    return Math.floor(Math.random()*4);
+}
+
+var x=generateRomderNumber();
+var y=generateRomderNumber();
+
+var graphics = new PIXI.Graphics();
+graphics.beginFill(0xeceffe, 1);
+graphics.drawRect(app.renderer.width/12+x*210, app.renderer.height/3+y*210, 200, 200);
+app.stage.addChild(graphics);
+
+var number = new PIXI.Text('2',{
+    fontSize: 180,
+    fill: '#adb4d2'
+});
+number.anchor.set(0.5);
+number.x = app.renderer.width/12+x*210+100;
+number.y = app.renderer.height/3+y*210+100;
+app.stage.addChild(number);
